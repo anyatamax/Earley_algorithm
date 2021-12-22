1c3c2f4d62a7787093bbdc141f9d1d2dd0acbd07
anya <anyata.maksimova@gmail.com>
2021-12-22 20:19:41 +0300

    improve Predict

--- include/Earley_algorithm.h
+++ include/Earley_algorithm.h
@@ -35,106 +35,14 @@ protected:
 
 class Algo {
 public:
-    void PredictWrapper(Grammar &grammar, std::string &str) {
-        if (FindString(grammar, str)) {
-            std::cout << "YES\n";
-        } else {
-            std::cout << "NO\n";
-        }
-    }
-    bool FindString(Grammar &grammar, std::string &str) {
-        grammar_ = grammar.GetGrammar();
-        cur_str_ = str;
-        int len_str = str.size();
-        state_table_.resize(len_str + 1);
-        std::set<Rule> start_rule;
-        start_rule.insert({'!', "S", 0, 0});
-        state_table_[0] = start_rule;
-        for (int i = 0; i <= len_str; ++i) {
-            std::set<Rule> new_sets = Scan(i);
-            for (auto &rule : new_sets) {
-                state_table_[i].insert(rule);
-            }
-            bool change = false;
-            do {
-                change = false;
-                new_sets = Complete(i);
-                for (auto &rule : new_sets) {
-                    if (state_table_[i].find(rule) == state_table_[i].end()) {
-                        state_table_[i].insert(rule);
-                        change = true;
-                    }
-                }
-                new_sets = Predict(i);
-                for (auto &rule : new_sets) {
-                    if (state_table_[i].find(rule) == state_table_[i].end()) {
-                        state_table_[i].insert(rule);
-                        change = true;
-                    }
-                }
-            } while (change);
-        }
-        Rule finish_rule = {'!', "S", 0, 1};
-        return (state_table_[len_str].find(finish_rule) != state_table_[len_str].end());
-    }
-    std::set<Rule> Scan(int index) {
-        std::set<Rule> find_sets;
-        if (index == 0) {
-            return find_sets;
-        }
-        for (auto &situation : state_table_[index - 1]) {
-            if (situation.dot_position < situation.right_path.size()) {
-                char symbol = situation.right_path[situation.dot_position];
-                if (IsTerminal(symbol) && symbol == cur_str_[index - 1]) {
-                    Rule new_rule = situation;
-                    ++new_rule.dot_position;
-                    find_sets.insert(new_rule);
-                }
-            }
-        }
-        return find_sets;
-    }
-    std::set<Rule> Predict(int index) {
-        std::set<Rule> find_sets;
-        for (auto &situation : state_table_[index]) {
-            if (situation.dot_position < situation.right_path.size()) {
-                char symbol = situation.right_path[situation.dot_position];
-                if (IsNonTerminal(symbol)) {
-                    for (auto &grammar_rule : grammar_[symbol]) {
-                        Rule new_rule = {symbol, grammar_rule, index, 0};
-                        find_sets.insert(new_rule);
-                    }
-                }
-            }
-        }
-        return find_sets;
-    }
-    std::set<Rule> Complete(int index) {
-        std::set<Rule> find_sets;
-        for (auto &situation : state_table_[index]) {
-            if (situation.dot_position == situation.right_path.size()) {
-                int parent_id = situation.index_parent;
-                char transition = situation.left_path;
-                for (auto &complete_situation : state_table_[parent_id]) {
-                    if (complete_situation.dot_position < complete_situation.right_path.size()) {
-                        if (complete_situation.right_path[complete_situation.dot_position] == transition) {
-                            Rule new_rule = complete_situation;
-                            ++new_rule.dot_position;
-                            find_sets.insert(new_rule);
-                        }
-                    }
-                }
-            }
-        }
-        return find_sets;
-    }
-    static bool IsNonTerminal(char letter) {
-        return std::isupper(letter);
-    }
-
-    static bool IsTerminal(char letter) {
-        return std::islower(letter);
-    }
+    explicit Algo(Grammar &grammar) : grammar_(grammar.GetGrammar()){};
+    void PredictWrapper(std::string &str);
+    bool FindString(std::string &str);
+    std::set<Rule> Scan(int index);
+    std::set<Rule> Predict(int index);
+    std::set<Rule> Complete(int index, std::set<Rule> &new_situations);
+    static bool IsNonTerminal(char letter);
+    static bool IsTerminal(char letter);
 
 private:
     std::map<char, std::vector<std::string>> grammar_;
--- src/Earley_algorithm.cpp
+++ src/Earley_algorithm.cpp
@@ -70,3 +70,111 @@ std::istream& operator>>(std::istream& in, Grammar& grammar) {
     grammar.InsertGrammar(rule[0], rule.substr(index, rule.size()));
     return in;
 }
+
+void Algo::PredictWrapper(std::string &str) {
+    if (FindString(str)) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+}
+bool Algo::FindString(std::string &str) {
+    cur_str_ = str;
+    int len_str = str.size();
+    state_table_ = std::vector<std::set<Rule>>(len_str + 1);
+    std::set<Rule> start_rule;
+    start_rule.insert({'!', "S", 0, 0});
+    state_table_[0] = start_rule;
+    for (int i = 0; i <= len_str; ++i) {
+        std::set<Rule> new_sets = Scan(i);
+        std::set<Rule> new_sets_complete;
+        std::set<Rule> new_sets_predict;
+        for (auto &rule : new_sets) {
+            state_table_[i].insert(rule);
+        }
+        bool change = false;
+        bool flag_first = true;
+        do {
+            change = false;
+            if (flag_first) {
+                new_sets_complete = Complete(i, state_table_[i]);
+            } else {
+                new_sets_complete = Complete(i, new_sets_complete);
+            }
+            for (auto &rule : new_sets_complete) {
+                if (state_table_[i].find(rule) == state_table_[i].end()) {
+                    state_table_[i].insert(rule);
+                    change = true;
+                }
+            }
+            new_sets = Predict(i);
+            for (auto &rule : new_sets) {
+                if (state_table_[i].find(rule) == state_table_[i].end()) {
+                    state_table_[i].insert(rule);
+                    change = true;
+                }
+            }
+            flag_first = false;
+        } while (change);
+    }
+    Rule finish_rule = {'!', "S", 0, 1};
+    return (state_table_[len_str].find(finish_rule) != state_table_[len_str].end());
+}
+std::set<Rule> Algo::Scan(int index) {
+    std::set<Rule> find_sets;
+    if (index == 0) {
+        return find_sets;
+    }
+    for (auto &situation : state_table_[index - 1]) {
+        if (situation.dot_position < situation.right_path.size()) {
+            char symbol = situation.right_path[situation.dot_position];
+            if (IsTerminal(symbol) && symbol == cur_str_[index - 1]) {
+                Rule new_rule = situation;
+                ++new_rule.dot_position;
+                find_sets.insert(new_rule);
+            }
+        }
+    }
+    return find_sets;
+}
+std::set<Rule> Algo::Predict(int index) {
+    std::set<Rule> find_sets;
+    for (auto &situation : state_table_[index]) {
+        if (situation.dot_position < situation.right_path.size()) {
+            char symbol = situation.right_path[situation.dot_position];
+            if (IsNonTerminal(symbol)) {
+                for (auto &grammar_rule : grammar_[symbol]) {
+                    Rule new_rule = {symbol, grammar_rule, index, 0};
+                    find_sets.insert(new_rule);
+                }
+            }
+        }
+    }
+    return find_sets;
+}
+std::set<Rule> Algo::Complete(int index, std::set<Rule> &new_situations) {
+    std::set<Rule> find_sets;
+    for (auto &situation : new_situations) {
+        if (situation.dot_position == situation.right_path.size()) {
+            int parent_id = situation.index_parent;
+            char transition = situation.left_path;
+            for (auto &complete_situation : state_table_[parent_id]) {
+                if (complete_situation.dot_position < complete_situation.right_path.size()) {
+                    if (complete_situation.right_path[complete_situation.dot_position] == transition) {
+                        Rule new_rule = complete_situation;
+                        ++new_rule.dot_position;
+                        find_sets.insert(new_rule);
+                    }
+                }
+            }
+        }
+    }
+    return find_sets;
+}
+bool Algo::IsNonTerminal(char letter) {
+    return std::isupper(letter);
+}
+
+bool Algo::IsTerminal(char letter) {
+    return std::islower(letter);
+}
